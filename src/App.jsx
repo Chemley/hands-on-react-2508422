eff999e79b5f8a650e547096f11d8021083c486a
Cana Owens <canaowens@gmail.com>
2023-05-30 17:43:05 +0000

    Added hooks and class example

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import React, { Component } from 'react';
+
+
+// class
+class Welcome extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <h1>Meet the { this.props.name }</h1>
+    )
+  }
+}
+
+// hooks
+const SayHello = (props) => {
+  return <h1>Meet the <i>{props.name}</i></h1>
+}
+
 function App() {
-  const name = 'StarGazers'
   return (
     <div className="container">
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
+          <Welcome name="StarGazers"/>
+          <Welcome name="Stars"/>
+          <SayHello name="magic" />
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
